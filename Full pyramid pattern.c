07ea893ab77213e0886cf87dab98aeac0f1c7b51
Eslam Tohamy <117383087+eslamtohamy49@users.noreply.github.com>
2023-10-19 12:47:07 +0300

    Add files via upload

---
+++ Full pyramid pattern.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+unsigned int Rows_Number   = 0;
+unsigned int Counter_One   = 0;
+unsigned int Counter_Two   = 0;
+unsigned int Counter_Three = 0;
+
+printf("Enter the Number of the Rows :");
+scanf("%i",&Rows_Number);
+
+
+for(Counter_One =0 )
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
